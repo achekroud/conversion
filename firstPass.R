7aecb25baacee155555638163a550e1608a6a8d5
Adam Chekroud <adamchekroud@gmail.com>
2016-11-28 14:08:05 -0500

    sops sums

--- firstPass.R
+++ firstPass.R
@@ -3,14 +3,13 @@
 
 
 #### Housekeeping
-
 # Load libraries
 libs <- c("RColorBrewer", "ape", "dynamicTreeCut", "cluster",
           "doMC", "readxl", "stringr","foreach", "tidyverse")
 invisible(lapply(libs, require, character.only = TRUE))
 registerDoMC(detectCores()-1)
 
-### Directories and seeds
+# Directories and seeds
 set.seed(1) # Reading from the same sheet
 setwd("~/Documents/PhD/Projects/conversion")
 workDir  <- getwd()
@@ -19,11 +18,12 @@ dataDir  <- file.path(workDir, "data")
 source("cannon_cluster_functions.R")
 # list.files(dataDir)
 
-keepVisits <- c(1,3,5)
+keepVisits <- c(1:5)
 
 
 pop_raw <- file.path(dataDir, "napls_pops.xlsx") %>% read_excel()
 pop     <- pop_raw %>%
+    filter(SubjectType == "Prodromal") %>%
     mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
     mutate(RID = paste(SiteNumber,SubjectNumber,VisitNumber, sep="_")) %>%
     select(ID, pops_criteria_met)
@@ -32,32 +32,54 @@ pop     <- pop_raw %>%
 
 gaf_raw <- file.path(dataDir, "napls_gaf.xlsx") %>% read_excel()
 gaf     <- gaf_raw %>% 
+    filter(SubjectType == "Prodromal") %>%
     filter(VisitNumber %in% keepVisits) %>%
     mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
     mutate(RID = paste(SiteNumber,SubjectNumber,VisitNumber, sep="_")) %>%
     select(RID, ID, VisitNumber, gaf_CurrentScore)
 
 
-cog_raw <- file.path(dataDir, "napls_matrics.xlsx") %>% read_excel()
-cog     <- cog_raw %>% 
-    filter(VisitNumber %in% keepVisits) %>% 
-    mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
-    mutate(RID = paste(SiteNumber,SubjectNumber,VisitNumber, sep="_")) %>%
-    select(RID,ID, VisitNumber, matrics_tmt_ptile)
+# cog_raw <- file.path(dataDir, "napls_matrics.xlsx") %>% read_excel()
+# cog     <- cog_raw %>% 
+#     filter(SubjectType == "Prodromal") %>%
+#     filter(VisitNumber %in% keepVisits) %>% 
+#     mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
+#     mutate(RID = paste(SiteNumber,SubjectNumber,VisitNumber, sep="_")) %>%
+#     select(RID,ID, VisitNumber, matrics_tmt_ptile)
 
 
 sop_raw   <- file.path(dataDir, "napls_sops.xlsx") %>% read_excel()
 sop       <- sop_raw %>% 
+    filter(SubjectType == "Prodromal") %>%
     filter(VisitNumber %in% keepVisits) %>% 
     mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
     mutate(RID = paste(SiteNumber,SubjectNumber,VisitNumber, sep="_")) %>%
-    select(RID, ID, VisitNumber, ends_with("_SOPS"))
+    select(RID, ID, VisitNumber, ends_with("_SOPS")) %>%
+    mutate(Px_tot = (P1_SOPS + P2_SOPS + P3_SOPS + P4_SOPS + P5_SOPS),
+           Nx_tot = (N1_SOPS + N2_SOPS + N3_SOPS + N4_SOPS + N5_SOPS + N6_SOPS),
+           Gx_tot = (G1_SOPS + G2_SOPS + G3_SOPS + G4_SOPS),
+           Dx_tot = (D1_SOPS + D2_SOPS + D3_SOPS + D4_SOPS)) %>%
+    select(RID,ID, VisitNumber, Px_tot, Nx_tot, Gx_tot, Dx_tot)
 
-sop$SOP_t <- sop %>% select(ends_with("_SOPS")) %>% rowSums()
-sop       <- sop %>% select(-ends_with("_SOPS"))
+# sop$SOP_t <- sop %>% select(ends_with("_SOPS")) %>% rowSums()
+# sop       <- sop %>% select(-ends_with("_SOPS"))
+
+gfs_raw   <- file.path(dataDir, "napls_gfs.xlsx") %>% read_excel()
+gfs       <- gfs_raw %>%
+    filter(SubjectType == "Prodromal") %>%
+    filter(VisitNumber %in% keepVisits) %>% 
+    mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
+    mutate(RID = paste(SiteNumber,SubjectNumber,VisitNumber, sep="_")) %>%
+    select(RID,ID, VisitNumber, gfs_current)
 
 
-
+gfr_raw   <- file.path(dataDir, "napls_gfr.xlsx") %>% read_excel()
+gfr       <- gfr_raw %>%
+    filter(SubjectType == "Prodromal") %>%
+    filter(VisitNumber %in% keepVisits) %>% 
+    mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
+    mutate(RID = paste(SiteNumber,SubjectNumber,VisitNumber, sep="_")) %>%
+    select(RID,ID, VisitNumber, gfr_current)
 
 
 
@@ -141,12 +163,21 @@ cog_raw %>%
     ggplot() +
     geom_violin(aes(x= factor(cluster), y = matrics_cptip_tscore))
 
-# 
-# # ditch matrics
+
+# ditch matrics
+## done
+
 # gfs means gf_social gfr means gf-role
+
 # only keep prodromes not controls
+## done
+
 # for function use gfs_current and gfr_current for each time pt
 #      check which one of these has the highest range withing subject over time
+
+## done, GFR 
+
+
 # for symptoms use SOPS sum(P1-P5) (may as well also calculate N1-Nx and G1-Gx and D1-Dx but dont use them yet)
 # try and include timepoints 2 and 4 now that we excluded matrics
 # lets flip SOPS because currently, high SOPS means bad. So if we flip it then high SOPS will be good (consistent with the other scales). so a 0 SOPS should become a 6. ie. (SOPS * -1) + 6 
