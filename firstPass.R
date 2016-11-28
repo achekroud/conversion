329fe3a3fb74e1c4abb247b839d210992339b343
Adam Chekroud <adamchekroud@gmail.com>
2016-11-28 13:37:29 -0500

    checkpoint before i do those suggestions from ty

--- firstPass.R
+++ firstPass.R
@@ -94,15 +94,25 @@ all$cluster <- out$cut
 
 temp <- full_join(all, pop, by = "ID")
 
+
+dev.off()
+setEPS(); postscript("gaf_traj_by_cluster.eps")
 gaf_raw %>% 
     mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
     select(ID, VisitNumber, gaf_CurrentScore) %>%
     left_join(., select(temp, ID, cluster), by = "ID") %>% 
-    group_by(VisitNumber, cluster) %>%
-    summarise(meanGAF = mean(gaf_CurrentScore, na.rm=TRUE)) %>%
+    mutate(visit = plyr::mapvalues(VisitNumber, 
+                             from = c(1,2,3,4,5,6),
+                             to = c(0,6,12,18,24,30) ) ) %>%
+    group_by(visit, cluster) %>%
+    summarise(meanGAF = mean(gaf_CurrentScore, na.rm=TRUE), count = n()) %>%
     ggplot() +
-    geom_line(aes(x=VisitNumber, y= meanGAF, color = factor(cluster)))
+    geom_line(aes(x=visit, y= meanGAF, color = factor(cluster)))
+dev.off()
 
+
+dev.off()
+png("last_gaf_by_cluster.png")
 gaf_raw %>%
     mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
     select(ID, VisitNumber, gaf_CurrentScore) %>%
@@ -112,7 +122,13 @@ gaf_raw %>%
     group_by(ID) %>%
     summarise_each(funs(last)) %>%
     ggplot() +
-    geom_violin(aes(x= factor(cluster), y = gaf_CurrentScore))
+    geom_violin(aes(x= factor(cluster), y = gaf_CurrentScore, fill = factor(cluster)),
+                alpha=0.5) +
+    geom_boxplot(aes(x= factor(cluster), y = gaf_CurrentScore),
+                 width=0.1, fill="white", outlier.size=0, alpha=0.5) +
+    ggtitle("Last available GAF_current score by cluster")
+dev.off()
+
 
 cog_raw %>%
     mutate(ID = paste(SiteNumber,SubjectNumber, sep="_")) %>%
@@ -124,5 +140,24 @@ cog_raw %>%
     summarise_each(funs(last)) %>%
     ggplot() +
     geom_violin(aes(x= factor(cluster), y = matrics_cptip_tscore))
+
+# 
+# # ditch matrics
+# gfs means gf_social gfr means gf-role
+# only keep prodromes not controls
+# for function use gfs_current and gfr_current for each time pt
+#      check which one of these has the highest range withing subject over time
+# for symptoms use SOPS sum(P1-P5) (may as well also calculate N1-Nx and G1-Gx and D1-Dx but dont use them yet)
+# try and include timepoints 2 and 4 now that we excluded matrics
+# lets flip SOPS because currently, high SOPS means bad. So if we flip it then high SOPS will be good (consistent with the other scales). so a 0 SOPS should become a 6. ie. (SOPS * -1) + 6 
+# also jam them into a 0-6 scale i.e. for gfs and gfr you can *6 and divide by 10
+
+
+
+
+
+
+
+
 
 
